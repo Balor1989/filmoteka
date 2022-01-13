56d5c6db3090c0e04f5422dca6487083966a4982
Olena <86402764+mikhailova11@users.noreply.github.com>
2022-01-13 22:17:48 +0200

    осуществлена функция закрытия и открытия модального окна

--- src/js/refs/variables.js
+++ src/js/refs/variables.js
@@ -2,6 +2,10 @@
      API_KEY: '7f7f3cc03c05575ccb98184b93174d1e',
      page: 1,
      rootEl: document.querySelector('.filmlist'),
+     modalInfo: document.querySelector('.modal__detail'),
+     onModalCloseBtn: document.querySelector('.button-close'),
+     onModalWatchedBtn: document.querySelector('.btn__color'),
+     onModalQueueBtn: document.querySelector('.btn__light'),
 };
 
 export default refs
