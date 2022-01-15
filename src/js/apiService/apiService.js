dcb025706f133d3d9db5892dd7cb39e4ce01b4b2
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-15 16:30:40 +0200

    change page position

--- src/js/apiService/apiService.js
+++ src/js/apiService/apiService.js
@@ -3,7 +3,8 @@ import fetchGenresOfMovie from './fetchGenresOfMovie';
 import fetchMovies from './fetchMovies';
 import refs from '../refs/variables';
 
-const { API_KEY, page } = refs;
+const { API_KEY } = refs;
+let page = 1
 
 function fetchPopularMovies() {
     fetchGenresOfMovie(API_KEY)
