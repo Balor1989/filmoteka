1286c5154ed31c0f94ecf65cb11182437ddd93b8
AndrieievaLena <elenakovalevamod@gmail.com>
2022-01-16 04:46:56 +0200

    Added modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './sass/main.scss';
 import fetchPopularMovies from './js/apiService/apiService'
 import backToTop from './js/plugins/backToTop'
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
-import libraryClickBtn from './js/library';
+import libraryClickBtn from './js/library'
+
 
 
 backToTop()
