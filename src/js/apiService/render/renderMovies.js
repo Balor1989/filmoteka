84e3db7edce78d5bc6b1e4224206ed19c177b259
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-20 23:19:22 +0200

    Update renderMovies.js

--- src/js/apiService/render/renderMovies.js
+++ src/js/apiService/render/renderMovies.js
@@ -1,43 +1,40 @@
 import refs from '../../refs/variables';
-import movieCard from '../../../templates/movieCard.hbs'
+import movieCard from '../../../templates/movieCard.hbs';
 import { genres } from '../fetch/fetchGenresOfMovie';
 
-const { rootEl } = refs
+const { rootEl } = refs;
 
 function renderMovies(response) {
-    const data = response.data.results
+    const data = response.data.results;
         //  Notify.success('Succes');
-       console.log(genres)
+    console.log(genres);
 
-        const movies = data.map(
-            (movie) => {
-                const { release_date, poster_path, genre_ids } = movie
-                let genresID = genre_ids
-                let date = release_date
-                if (genresID && genresID.length > 2) {
-                     genresID.splice(2, 5, "other")
-                }
-                if (date) {
-                    date=release_date.slice(0, 4)
-                }
-                if (!date) {
-                    date='????'
-                }
-                let genresOfMovie = genresID.map(id => id === "other" ? 'Other' : genres[id]).join(', ');
-                return {
-                    ...movie,
-                    release_date:date,
-                    img: poster_path,
-                    genre_ids: genresOfMovie
-                };
-         }
-        )
-        console.log(movies)
-    
-    const markup = movies.map(movieCard).join('')
-        rootEl.innerHTML=''
-        rootEl.insertAdjacentHTML('beforeend', markup)
+    const movies = data.map(
+        (movie) => {
+            const { release_date, poster_path, genre_ids } = movie;
+            let genresID = genre_ids;
+            let date = release_date;
+
+            if (genresID && genresID.length > 2) {
+                genresID.splice(2, 5, "other")
+            };
+            if (date) {
+                date = release_date.slice(0, 4)
+            };
+
+            let genresOfMovie = genresID.map(id => id === "other" ? 'Other' : genres[id]).join(', ');
+            return {
+                ...movie,
+                release_date: date || 'unknown year',
+                img: poster_path,
+                genre_ids: genresOfMovie || 'other'
+            };
+        }
+    );
+   
+    const markup = movies.map(movieCard).join('');
+    rootEl.innerHTML = '';
+    rootEl.insertAdjacentHTML('beforeend', markup);
 }
 
-
-export default renderMovies
+export default renderMovies;
