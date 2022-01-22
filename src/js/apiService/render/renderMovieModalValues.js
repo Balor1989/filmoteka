f9f96bdc86431c096164081825a45e46f96bc29a
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-22 15:17:17 +0200

    add toggle button

--- src/js/apiService/render/renderMovieModalValues.js
+++ src/js/apiService/render/renderMovieModalValues.js
@@ -9,7 +9,7 @@ export default function rerenderMovieDetails(response) {
 
     const modalWatchedButton = document.querySelector('.btn__watched');
     const modalQueueButton = document.querySelector('.btn__queue');
-
+     
     modalWatchedButton.addEventListener('click', onClickWatchedButton);
     modalQueueButton.addEventListener('click', onClickQueueButton);
 }
@@ -18,9 +18,13 @@ export default function rerenderMovieDetails(response) {
 
 function onClickWatchedButton(e) {
     console.log('click Watched Button')
+    const modalWatchedButton = document.querySelector('.btn__watched')
+    modalWatchedButton.classList.toggle('btn__watched--active')
 
     const currentMovie = JSON.parse(localStorage.getItem('movie'));
     const watchedMovies = JSON.parse(localStorage.getItem('watched')) || [];
+
+     
 
     if (watchedMovies.find(watchedMovie => watchedMovie.id === currentMovie.id)) {
         
@@ -33,11 +37,17 @@ function onClickWatchedButton(e) {
     localStorage.setItem('watched', JSON.stringify(watchedMovies));
 }
 
+
+
 function onClickQueueButton(e) {
     console.log('click Queue Button');
 
+    const modalQueueButton = document.querySelector('.btn__queue');
+    modalQueueButton.classList.toggle('btn__queue--active')
+
     const currentMovie = JSON.parse(localStorage.getItem('movie'));
     const queueMovies = JSON.parse(localStorage.getItem('queue')) || [];
+ 
 
     if (queueMovies.find(queueMovie => queueMovie.id === currentMovie.id)) {
         
