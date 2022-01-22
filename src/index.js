b7a4e15605b0ceeaf7bfa6009f745d3f8e0a9b69
Pavel <Pavel.Debchuk@gmail.com>
2022-01-22 15:53:45 +0300

    btn colorTheme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './js/plugins/tui-pagination';
 import renderPopularMovies from './js/apiService/apiService';
 import backToTop from './js/plugins/backToTop';
 import libraryClickBtn from './js/library';
-
+import './js/themeBtn';
 
 backToTop()
 
