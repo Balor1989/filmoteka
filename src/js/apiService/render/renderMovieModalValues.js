cebb107501d94d401012228b7122296037992103
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-22 14:18:03 +0200

    adds remove movie on click

--- src/js/apiService/render/renderMovieModalValues.js
+++ src/js/apiService/render/renderMovieModalValues.js
@@ -6,28 +6,46 @@ const { modalInfo } = refs;
 
 export default function rerenderMovieDetails(response) {
     modalInfo.insertAdjacentHTML('afterbegin', movieDetailsTpl(response));
-    const modalWatchedButton = document.querySelector('.btn__watched')
-    const modalQueueButton = document.querySelector('.btn__queue')
-    modalWatchedButton.addEventListener('click', onClickWatchedButton)
-    modalQueueButton.addEventListener('click', onClickQueueButton)
+
+    const modalWatchedButton = document.querySelector('.btn__watched');
+    const modalQueueButton = document.querySelector('.btn__queue');
+
+    modalWatchedButton.addEventListener('click', onClickWatchedButton);
+    modalQueueButton.addEventListener('click', onClickQueueButton);
 }
 
 
 
 function onClickWatchedButton(e) {
     console.log('click Watched Button')
-    const currentMovie = JSON.parse(localStorage.getItem('movie'))
+
+    const currentMovie = JSON.parse(localStorage.getItem('movie'));
     const watchedMovies = JSON.parse(localStorage.getItem('watched')) || [];
-    console.log(watchedMovies)
-    watchedMovies.push(currentMovie)
-    localStorage.setItem('watched', JSON.stringify(watchedMovies))
+
+    if (watchedMovies.find(watchedMovie => watchedMovie.id === currentMovie.id)) {
+        
+        localStorage.setItem('watched', JSON.stringify(watchedMovies.filter(watchedMovie => watchedMovie.id !== currentMovie.id)));
+        return;
+    }
+    
+    watchedMovies.push(currentMovie);
+
+    localStorage.setItem('watched', JSON.stringify(watchedMovies));
 }
 
 function onClickQueueButton(e) {
-    console.log('click Queue Button')
-    const currentMovie = JSON.parse(localStorage.getItem('movie'))
-    const queueMovies = JSON.parse(localStorage.getItem('queue')) || []
-    console.log(queueMovies)
-    queueMovies.push(currentMovie)
-    localStorage.setItem('queue', JSON.stringify(queueMovies))
+    console.log('click Queue Button');
+
+    const currentMovie = JSON.parse(localStorage.getItem('movie'));
+    const queueMovies = JSON.parse(localStorage.getItem('queue')) || [];
+
+    if (queueMovies.find(queueMovie => queueMovie.id === currentMovie.id)) {
+        
+        localStorage.setItem('queue', JSON.stringify(queueMovies.filter(queueMovie => queueMovie.id !== currentMovie.id)));
+        return;
+    }
+
+    queueMovies.push(currentMovie);
+
+    localStorage.setItem('queue', JSON.stringify(queueMovies));
 }
