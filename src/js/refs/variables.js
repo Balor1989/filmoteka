3cc695bfe188186c13502327e0a4d89bc0ecbd27
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-23 22:02:02 +0200

    refactoring modal

--- src/js/refs/variables.js
+++ src/js/refs/variables.js
@@ -1,11 +1,14 @@
- const refs = {
-     API_KEY: '7f7f3cc03c05575ccb98184b93174d1e',
-     rootEl: document.querySelector('.filmlist'),
-     modalInfo: document.querySelector('.modal__detail'),
-     onModalWatchedBtn: document.querySelector('.btn__watched'),
-     onModalQueueBtn: document.querySelector('.btn__queue'),
-     btnSearch: document.querySelector('.button'),
-     inputName: document.querySelector('.input')  
+const refs = {
+    API_KEY: '7f7f3cc03c05575ccb98184b93174d1e',
+    rootEl: document.querySelector('.filmlist'),
+    modalInfo: document.querySelector('.modal__detail'),
+    onModalWatchedBtn: document.querySelector('.btn__watched'),
+    onModalQueueBtn: document.querySelector('.btn__queue'),
+    btnSearch: document.querySelector('.button'),
+    inputName: document.querySelector('.input'),
+    openModal: document.querySelector('.filmlist'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    footerOpenModalBtn: document.querySelector(".footer-box_line")
 };
 
-export default refs
+export default refs;
