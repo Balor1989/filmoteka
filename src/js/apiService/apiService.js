b2c434f236ee8aaddad28a6a8e686070b34ce0f6
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-23 18:55:05 +0200

    adds docs to apiService

--- src/js/apiService/apiService.js
+++ src/js/apiService/apiService.js
@@ -5,49 +5,54 @@ import debounce from "lodash.debounce";
 import refs from '../refs/variables';
 import pagination from '../plugins/tui-pagination';
 
+
 const { API_KEY, inputName } = refs;
- export let page = 1
+export let page = 1;
 
 
 // Search movies by user request
 // Runs with a handler by input value to the search 
 const onFetchMovieByName = (e) => {
     if (!e.target.value) {
-        fetchPopularMovies(API_KEY, page=1);
+        fetchPopularMovies(API_KEY, page = 1);
         return;
-    }
-    console.log(e.target.value)
-    fetchMoviesByName(API_KEY, page=1, inputName.value)
+    };
+
+    fetchMoviesByName(API_KEY, page = 1, inputName.value);
     
-}
+};
 
 // Handlers
-inputName.addEventListener('input', debounce( onFetchMovieByName, 300))
+inputName.addEventListener('input', debounce(onFetchMovieByName, 300));
+
+
 
 // Search for popular films. 
 // Loads a list of movies on the main page and also adds genres to the "genres" variable
 async function renderPopularMovies() {
-    await fetchGenresOfMovie(API_KEY)
-   await fetchPopularMovies(API_KEY, page)
-}
 
-export default renderPopularMovies
+    await fetchGenresOfMovie(API_KEY);
+
+    await fetchPopularMovies(API_KEY, page);
+};
 
 
-
+// If the input value is 'false' - renders popular movies, otherwise - movies on demand
 function usePagination(event) {
     page = event.page;
-    console.log(event)
+
     if (inputName.value) {
-        fetchMoviesByName(API_KEY, page, inputName.value)
+        fetchMoviesByName(API_KEY, page, inputName.value);
         return;
-    }
+    };
+
     if (!inputName.value) {
-        fetchPopularMovies(API_KEY, page)
-    }
-}
+        fetchPopularMovies(API_KEY, page);
+    };
+};
 
-
+// Tui-pagination options
+// Adds a scroll effect after generating a new page
 pagination.on('afterMove', (event) => {
 
     window.scrollTo({
@@ -56,7 +61,10 @@ pagination.on('afterMove', (event) => {
     });
 });
 
+// Render movies by condition
 pagination.on('beforeMove', (event) => {
     usePagination(event);
 });
 
+
+export default renderPopularMovies;
