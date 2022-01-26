13a7598652ec7564208f279efb5b8b778e6c71dc
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-27 00:32:56 +0200

    fixs bugs

--- src/js/apiService/fetch/fetchGenresMenu.js
+++ src/js/apiService/fetch/fetchGenresMenu.js
@@ -1,21 +1,23 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import refs from '../../refs/variables';
-import render from "../render/renderGenresMenu";
-
-
-const  { renderGenresMenu, renderElements} = render;
+import renderMovies from '../render/renderMovies';
+import axios from "axios";
+import renderGenresMenu from '../render/renderGenresMenu';
+import pagination from '../../plugins/tui-pagination';
+import { page } from '../apiService';
 
 const { API_KEY} = refs;
 
-const dropdown = document.querySelector('.dropdown-content')
 
-let page = "";
+const dropdown = document.querySelector('.dropdown-content');
 
-dropdown.addEventListener('click',  searchByGenre)
+dropdown.addEventListener('click', searchByGenre);
 
 export default async function fetchGenresMenu() {
     try { fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-        .then(response => {return response.json()})
+        .then(response => {
+            return response.json();
+        })
         .then( renderGenresMenu)
     } catch (error) {
         Notify.failure(`${error}`);
@@ -24,26 +26,25 @@ export default async function fetchGenresMenu() {
 };
 
 
+   function searchByGenre(e) {
+       e.preventDefault();
 
-
-async function searchByGenre(e) {
-    e.preventDefault()
-    if (e.target.nodeName !== 'A') {return;}
-    const genreID = e.target.dataset.sources;
-
-    const objList = await fetchMovies(genreID);
-    await renderElements(objList.results, genreID);
-
+       if (e.target.nodeName !== 'A') {
+           return;
+       }
+       const genreID = e.target.dataset.sources;
+       fetchMovies(genreID);
 };
   
 async function fetchMovies(genreID) {
-    
-    try {const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreID}&api_key=${API_KEY}&page=${page}`);
-    return response.json()
+   
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreID}&api_key=${API_KEY}&page=${page}`);
+        renderMovies(response);
     } catch (error) {
         Notify.failure(`${error}`);
         return Promise.reject(error);
     };
-}   
+};
 
 
--- src/js/apiService/render/renderGenresMenu.js
+++ src/js/apiService/render/renderGenresMenu.js
@@ -1,75 +1,9 @@
 import genresMenuTpl  from "../../../templates/genresMenu.hbs";
-import refs from '../../refs/variables';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import pagination from '../../plugins/tui-pagination';
-import movieCardTpl from "../../../templates/movieCardGenr.hbs";
 
-let totalResults = 20000;  // Start value for pagination.
+const dropdown = document.querySelector('.dropdown-content');
 
-const { rootEl} = refs;
-const dropdown = document.querySelector('.dropdown-content')
+function renderGenresMenu(genre) {
+    dropdown.innerHTML = genresMenuTpl(genre)
+};
 
-function renderGenresMenu(genre) { dropdown.innerHTML = genresMenuTpl(genre)}
-
-async function renderElements(list, currentGenre) {
-    try {
-   
-      rootEl.innerHTML = '';
-      const itemList = list.filter(i => {
-      const arrID = i.genre_ids;
-      const chooseGenre = arrID.find(item => item == currentGenre);
-      return chooseGenre;
-  
-      });
-      renderMovies(itemList);
-  } catch (error) {
-      Notify.failure(`${error}`);
-      return Promise.reject(error);
-  };
-  }
-  
-  function renderMovies(response) {     
-      if (!response) {Notify.failure('There are no movies for your request');};
-  
-      if (totalResults !== response) {
-         totalResults = response;
-  
-         pagination.reset(totalResults);  
-     };
-  
-  
-      const movies = response.filter(movie =>  {
-              const { release_date, poster_path, genre_ids } = movie;
-              let genresID = genre_ids;
-              let date = release_date;
-              
-              if (genresID && genresID.length > 2) {
-                  genresID.splice(2, 5, "other");
-              };
-              if (date) {
-                  date = release_date.slice(0, 4);
-              };
-
-              let genresOfMovie = genresID.map(id => id === "other" ? 'Other' : movie[id]).join(', ');
-             
-              return {
-                  ...movie,
-                  release_date: date || 'unknown year',
-                  img: poster_path,
-                  genre_ids: genresOfMovie || 'other'
-              };
-          }
-      );
-      
-         
-      const markup = movies.map(movieCardTpl).join('');
-  
-      rootEl.innerHTML = '';
-  
-      rootEl.insertAdjacentHTML('beforeend', markup);
-      }
-
-      export default {
-        renderGenresMenu,
-        renderElements,
-      }
+export default renderGenresMenu;
