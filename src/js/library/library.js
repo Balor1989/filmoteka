752b0d941cfd4dbca0a4e4c59ddb067e8b8c92da
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-28 10:15:43 +0200

    delete reload when home button onClick

--- src/js/library/library.js
+++ src/js/library/library.js
@@ -3,17 +3,31 @@ import activeRenderWatchedMovies from './activeRenderWatchedMovies';
 import refs from '../refs/variables';
 
 const { listWatched, listQueue, buttonLib, buttonQueue, buttonWatched,
-  libraryHeader, mainHeader, mainLibrary, mainHome, paginationBox } = refs;
+  libraryHeader, mainHeader, mainLibrary, mainHome, paginationBox, libraryHomeButton } = refs;
 
 
+libraryHomeButton.addEventListener('click',onClickHomeButton )
 buttonLib.addEventListener('click', libraryClickBtn);
 buttonQueue.addEventListener('click', onButtonQueueClick);
 buttonWatched.addEventListener('click', onButtonWatchedClick);
 
+function onClickHomeButton(e) {
+e.preventDefault();
+
+  paginationBox.style.visibility = 'visible';
+  paginationBox.style.height = '190px';
+  libraryHeader.classList.add('hidden');
+  mainLibrary.classList.add('hidden');
+  mainHeader.classList.remove('hidden');
+  mainHome.classList.remove('hidden');
+
+};
+
 function libraryClickBtn(e) {
   e.preventDefault();
 
-  paginationBox.style.display = 'none'
+  paginationBox.style.visibility = 'hidden';
+  paginationBox.style.height = '0';
   libraryHeader.classList.remove('hidden');
   mainLibrary.classList.remove('hidden');
   mainHeader.classList.add('hidden');
@@ -22,7 +36,8 @@ function libraryClickBtn(e) {
   activeRenderWatchedMovies();
   activeRenderQueueMovies();
 
-}
+};
+
 
 function onButtonQueueClick() {
   listWatched.classList.add('hidden');
